00bfb5a7bf1bf62c6502615da835aa0a214dc217
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2024-01-24 23:24:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3002
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/sign', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
